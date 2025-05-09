b0362558e285b41499edb104b186ddfebbf0a42e
Gamze <gamz3akyuz@gmail.com>
2025-05-09 23:08:41 +0300

    Initial Commit

---
+++ ToDoList-SQLite/ToDoList_SQLiteApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoList_SQLiteApp.swift
+//  ToDoList-SQLite
+//
+//  Created by Gamze Akyüz on 9.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct ToDoList_SQLiteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
